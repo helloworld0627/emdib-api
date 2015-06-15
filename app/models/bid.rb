4f0453c311176ac99f0b0c69e877260de3a881c2
Chris Kwok <chris.kwok@climate.com>
2015-06-14 23:18:06 -0700

    add validation constraints for "Bid"

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,9 @@
 class Bid < ActiveRecord::Base
   belongs_to :auction
   belongs_to :business_owner
+
+  validates_presence_of :bid_status
+  validates_numericality_of :bid_price, greater_than: 0
+  validates_presence_of :auction
+  validates_presence_of :business_owner
 end
