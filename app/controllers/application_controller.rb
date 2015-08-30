de9358e61d299253cffa3530f274041f3c4fd19c
Chris Kwok <chris.kwok@climate.com>
2015-08-29 23:41:01 -0700

    support new user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,11 @@ class ApplicationController < ActionController::API
 	  	begin
 	  	  @graph = Koala::Facebook::API.new(oauth_access_token)
 	  	  profile = @graph.get_object("me")
-	  	  @user = User.find_by(client_id: profile["id"])
+	  	  if profile
+	  	  	profile_id = profile["id"]
+	  	  	rs = User.find_by(client_id: profile_id)
+	  	  	@user = (rs.nil?)? User.create!({client_id: profile_id}) : rs
+	  	  end
 	  	rescue Exception => e
 	  	  render :json => e, :status => e.http_status
 	  	end
